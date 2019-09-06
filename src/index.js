e06e22537cedef6237b03a3dd6fdd3eafeaf03b3
Vadim Gareev <standArtknight@gmail.com>
2019-09-06 13:55:15 +0500

    6 tasts

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@
    addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
  */
 function addListener(eventName, target, fn) {
+	target.addEventListener(eventName, fn);
 }
 
 /*
@@ -20,6 +21,7 @@ function addListener(eventName, target, fn) {
    removeListener('click', document.querySelector('a'), someHandler) // должна удалить указанный обработчик кликов на указанный элемент
  */
 function removeListener(eventName, target, fn) {
+	target.removeEventListener(eventName, fn);
 }
 
 /*
@@ -31,6 +33,7 @@ function removeListener(eventName, target, fn) {
    skipDefault('click', document.querySelector('a')) // после вызова функции, клики на указанную ссылку не должны приводить к переходу на другую страницу
  */
 function skipDefault(eventName, target) {
+	target.addEventListener(eventName, (event) => event.preventDefault());
 }
 
 /*
@@ -42,6 +45,9 @@ function skipDefault(eventName, target) {
    emulateClick(document.querySelector('a')) // для указанного элемента должно быть сэмулировано события click
  */
 function emulateClick(target) {
+    let event = new Event('click');
+    
+    target.dispatchEvent(event);
 }
 
 /*
@@ -54,6 +60,11 @@ function emulateClick(target) {
    delegate(document.body, () => console.log('кликнули на button')) // добавит такой обработчик кликов для body, который будет вызывать указанную функцию только если кликнули на кнопку (элемент с тегом button)
  */
 function delegate(target, fn) {
+	let buttonElem = target.getElementsByTagName('button');
+
+	for (const button of buttonElem) {
+		button.addEventListener('click', fn);
+	}
 }
 
 /*
@@ -66,6 +77,7 @@ function delegate(target, fn) {
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
+	target.addEventListener('click', fn, {once: true});
 }
 
 export {
