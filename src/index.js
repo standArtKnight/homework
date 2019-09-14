5c7cd8fd28bd1e2cf7d09eecba3e6418c829b41c
Vadim Gareev <standArtknight@gmail.com>
2019-09-15 00:23:25 +0500

    still working

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@
    delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
 function delayPromise(seconds) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, seconds * 1000);
+  });
 }
 
 /*
@@ -25,6 +28,17 @@ function delayPromise(seconds) {
    loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
  */
 function loadAndSortTowns() {
+  return new Promise((resolve, reject) => {
+    let result = [];
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
+    xhr.responseType = 'json';
+    xhr.send();
+    xhr.addEventListener('load', () => {
+      const cities = xhr.response;
+      resolve(cities.sort((a, b) => a.name.localeCompare(b.name)));
+    })
+  })
 }
 
 export {
